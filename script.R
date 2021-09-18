a38aa6cb6d9d1f4b22d9b9076cd3e3326c1327c6
Alex <alex.gombau@gmail.com>
2021-09-18 13:04:20 +0200

    First version

---
+++ script.R
@@ -0,0 +1,18 @@
+#import excel file to a data frame
+library("readxl")
+mtt_data<- read_excel("mtt_data_1.xlsx", 
+           col_names = FALSE)
+#dataframe to matrix
+mtt_matrix<- as.matrix(mtt_data)
+#function to relativize data
+relativize_data<- function(mtt_matrix)
+{
+  #mean of the control groups
+  mean(c(mtt_matrix[,2], mtt_matrix[,11]))
+  #relativize abs data
+  relative_mtt_data <- (mtt_matrix/pos_control_mean)*100
+  #remove not used wells
+  relative_means <- colMeans(relative_mtt_data[,c(-1,-12)])
+}
+#plot the data
+barplot(relative_means, main = "viability", xlab="ratio", ylab = "cell viability (%)", names.arg = c(rep("10:1",2), rep("8:1", 2), rep("5:1", 2), rep("2:1", 2), rep("Control", 2)))
